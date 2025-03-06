2456b3179ffd1c7d80e61b37df8d906c13e66053
Bright Robins <m_lowegren@mac.com>
2025-03-06 13:07:24 +0100

    Update index.py to use minimal_app

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
 # This file is used to redirect requests to the actual application
-from src.web_app import app
+from src.minimal_app import app
 
 # This is needed for Vercel to properly route requests 
